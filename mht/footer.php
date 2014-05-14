3c02ba0a76e61e94153ffac72b5d183bc96337e0
Mike <michael.kent@gmail.com>
2014-05-14 09:21:37 -0400

    Begining of the refactoring

---
+++ mht/footer.php
@@ -0,0 +1,7 @@
+<footer>
+	<ul class='nav'>
+		<li><a href="graph.php" id="nav-timeline">TimeLine</a></li>
+		<li><a href="survey.php" id="nav-survey">Survey</a></li>
+		<li><a href="settings.php" id="nav-settings">Settings</a></li>
+	</ul>
+</footer>
---
+++ mht/head.php
@@ -0,0 +1,15 @@
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width = device-width, initial-scale = 1, maximum-scale = 1.0">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<link rel="apple-touch-icon" href="images/57.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="images/114.png">
+		<!-- <link rel="apple-touch-startup-image" href="images/startup.png"> -->
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+		<title>MHT</title>
+		<link rel="stylesheet" media="screen" href="css/stylesheet.css">
+		<link rel="stylesheet" media="screen" href="css/graph-ui.css">
+		<link rel="stylesheet" media="screen" href="css/graph.css">
+		<link rel="stylesheet" media="screen" href="css/jquery-ui-styles.css">
+		<link rel="stylesheet" media="screen" href="css/graph-menu.css"> <!-- mk -->
+	</head>
