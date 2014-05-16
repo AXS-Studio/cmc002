1423a9bba3485ee255178844f7b6a2f20dc0d9cd
Mike <michael.kent@gmail.com>
2014-05-16 18:04:14 -0400

    New colour picker

--- mht/js/menu-ui.js
+++ mht/js/menu-ui.js
@@ -1,3 +1,4 @@
+/*
 $("#menubtn").live('click', function(e){
 	//if ($("#scs").css('display') == 'inline-block'){
 	//	$("#scs").css('display', 'none');
@@ -15,6 +16,9 @@ $("#menubtn").live('click', function(e){
 	}
 	return false;
 });
+*/
+
+
 
 var initGraphMenu =(function() {
 
@@ -48,6 +52,13 @@ var initGraphMenu =(function() {
 	$(".forgot").hide();
 	
 	$("footer .nav").show();
+	
+	
+	$('#legend_content').append('<h2>Survey Questions</h2><ul class="swatches" id="swtchs-legend"></ul>');
+	$('#edit_content').append('<h2>Survey Questions</h2><ul class="swatches" id="swtchs-edit"></ul>');
+	$('#share_content').append('<ul><li><button>Save As PDF</button></li><li><button>Email PDF</button></li></ul></div></ul></li>');
+	
+	GenerateSwatches.loadQuestions();
 	
 });
 
@@ -86,9 +97,9 @@ $('#art-loading').removeClass('o1').addClass('o0');
 			$('#page_settings').hide();
 			
 			//load initial data
-			initGraphMenu();
+			
 		 	DataVisualizationInitialization.loadDataInitial();
-
+		 	initGraphMenu();
 
 	
 
@@ -132,6 +143,7 @@ $('#art-loading').removeClass('o1').addClass('o0');
 });
 
 
+/*
 $("#scs li h2 a").live('click', function(e){
 	//$(".menu-section").css('display', 'none');
 	$("#scs li").removeClass('active');
@@ -149,4 +161,203 @@ $("#scs li h2 a").live('click', function(e){
 	}
 	return false;
 });
+*/
+
+
+
+
+ 	var GenerateSwatches = (function() {
+ 	
+		     //query_answers_initial.php was written originally for PATH, returns a set of default questions and their results
+		     //and for non-default questions, returns the length of their results so that null sets can be greyed out
+		     //Will need to rewrite the php for returning the entire bank of answers for use in the app as a one time download
+		     var loadQuestions = function() {
+		         $.ajax({
+		             url: 'php/query_questions.php',
+		             type: 'GET',
+		             dataType: 'json',
+		             success: function(response) {
+		                 questions = response;
+		                 console.log("questions", questions);
+		                 addSwatches();
+		             },
+		             complete: function () {
+			             
+			             AddColourPicker();
+		             },
+		             error: function() {
+		                 window.alert('Error loadQuestionsInitial!');
+		             }
+		         });
+
+		     };
+
+
+		     return {
+		         loadQuestions : loadQuestions
+		     };
+		 })();
+
+
+	function AddColourPicker () {
+		$("#swtchs-edit .swatches").spectrum({
+			color: "#f00",
+			change: function(color) {
+				$(this).css('background', color);
+				id = $(this).attr('id').replace("swatch-", "");
+				StoreColor(id,color)
+				
+			}
+		});
+		
+	}
+	
+        
+    var graphColors = new Array();
+	
+	function StoreColor(id,color) {
+		console.log("store request for " + id + " " + color);
+
+		for (i = 0; i < graphColors.length; i++ ){
+			if (graphColors[i].id == id) {
+				graphColors[i].color = color.toString();
+			}
+		}
+/* 		console.log(graphColors) */
+/* 		console.log(GetColor(id)); */
+	}
+	
+	function GetColor(id) {
+		for (i = 0; i < graphColors.length; i++ ){
+			if (graphColors[i].id == id) {
+				return graphColors[i].color;
+			}
+		}
+	}
+
+	function addSwatches() {	/* ALERT: Big problem in here regarding the swatches. */
+			
+			console.log(questions.length);
+			for (var i = 0; i < questions.length; i++) {
+				var category = questions[i].category;
+				/*  ALERT: Here's the problem with the items in the "OTHER" category: Two OTHER categories
+					are being included instead of one, so I'm not including the second OTHER object... */
+				if (category != 'Aggregate Scores' /* && (category != 'OTHER' || (category == 'OTHER' && otherCount == 0))*/) {
+					for (var j = 0; j < questions[i].type.length; j++) {
+						var id = questions[i].type[j].id;
+						var name = questions[i].type[j].name;
+						
+						graphColors.push({id : id, color : ""});
+						
+						$('#questions-legend').append('<li id="legend-'+id+'" style="display:none;"><span href="#" class="swatches" id="swatch-' + id + '"></span><span>' + name + '</span></li>');
+						$('#swtchs-legend').append('<li style="display:none;"><span class="swatches" id="swatch-' + id + '"></span><span>' + name + '</span></li>');
+						$('#swtchs-edit').append('<li><a href="#" title="Change ' + name + '\'s colour" class="swatches" id="swatch-' + id + '"></a><span>' + name + '</span></li>');
+					}
+				}
+
+				/*  ALERT: End OTHER fix.  */
+				if (category == 'Aggregate Scores') {
+					//$('#swtchs-edit').append('<li><a href="#" title="Change QIDS Score\'s colour" id="swatch-SCORE_0"></a> <span>QIDS Score</span></li>');
+					$('#questions-legend').append('<li id="legend-'+id+'" style="display:none;"><span>QIDS Score</span></li>');
+					$('#swtchs-legend').append('<li><span class="swatches" id="swatch-SCORE_0"></span> <span>QIDS Score</span></li>');
+					$('#swtchs-edit').append('<li><a href="#" title="Change ' + name + '\'s colour" id="swatch-' + id + '"></a> <span>' + name + '</span></li>');
+				}
+
+				// For each setting...
+				jQuery.each(settings, function(i, s) {
+					$('#swatch-' + s.id).parent().removeClass('disabled');
+
+					/* If the setting's colour is transparent, replace the swatches background colour with a transparency image... */
+					if (s.colour == 'transparent'){					
+						$('#menu-legend #swatch-' + s.id).css('background', 'url(images/visualizer_colour_select_transparent.gif)');
+						$('#menu-edit #swatch-' + s.id).css('background', 'url(images/visualizer_colour_select_transparent.gif)');
+						//$('#menu-legend #swatch-' + s.id).parent('li').remove();
+
+					// ...otherwise, use the setting's background colour
+					}else{ 
+						$('#swatch-' + s.id).css('background', s.colour);
+						$('#menu-edit #swatch-' + s.id).css('background', s.colour);
+						$('#legend-' + s.id + ' .swatches').css('background', s.colour);
+						$('#legend-' + s.id).css('display', 'inline-block');
+						$('#swtchs-legend #swatch-' + s.id).parent('li').css('display', 'block');
+					}
+
+					if (!$('#swatch-' + s.id).parent().hasClass('disabled')) {
+						// Apply ColorPicker plug-in to the swatch
+						$('#swtchs-edit #swatch-' + s.id).colorpicker({
+							alpha: true,
+							color: s.colour,
+							colorFormat: 'RGBA',
+							inline: false,
+							rgb: false,
+							hsv: false,
+							altAlpha: false,
+							preview: false,
+							select: function(event, color) {
+								sessionChanged = true;
+								var newColour = color.formatted;
+								var code = $(this).parents('ul.swatches').attr('id').split('-')[1];
+								var name = $(this).parent().children('span').html();
+								$(this).css('background', newColour);
+								if (newColour.split(',')[3] == '0)') {
+									newColour = 'transparent';
+									$(this).css('background', 'url(images/visualizer_colour_select_transparent.gif)');
+								}
+
+								$('.path-' + s.id).css('stroke', newColour);
+								$('.ma-' + s.id).css('stroke', newColour);
+								$('.path-' + s.id).css('fill', newColour);
+								$('.intLeg-' + s.id + ' div').css('background-color', newColour);
+								$('#legend-'+ s.id ).css('display', "inline-block");
+								$('#legend-'+ s.id ).css('display', "inline-block");
+								$('#legend-' + s.id + ' a').css('background', s.colour);
+								$('#swatch-' + s.id).css('background', s.colour);
+								$('#swtchs-legend #swatch-' + s.id).parent('li').css('display', 'block');
+
+								s.colour = newColour;
+								updateColourRefs(initialData, s.id, newColour);
+								updateColourRefs(settings, s.id, newColour);
+
+								// Ajax calls are going to go in here
+								jQuery.each(initialData, function(i, d) {
+										if (s.id == d.id) {
+											if (d.results == null) {
+												$.ajax({
+													type: 'GET',
+													url: 'php/query_answers.php',
+													data: {
+														"patientID": patient,
+														"questionID": d.id
+													},
+													dataType: 'json',
+													success: function(response) {
+														//console.log(response.results);
+														updateInitial(s.id, code + '_' + name, response.results);
+														// Reset the range1 and range2 brush settings
+														// Re-add the parent SVG's focus graph and context graph
+														addFocusContext();
+														// Update the integrals table
+													},
+													error: function() {
+														window.alert('Swatch Ajax error.');
+													}
+												});
+											}
+										}
+								});
+							},
+							close: function(event, color) {
+								var newColour = color.formatted;
+								updateColourRefs(initialData, s.id, newColour);
+								updateColourRefs(settings, s.id, newColour);
+							}
+						});
+					}
+				});
+				$('#scs ul.swatches li a').click(function() {
+					return false;
+				});
+			}
+/* 		} */
+	}	
 
