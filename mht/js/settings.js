6b2170ed2f5a1ecd6ad5a6483b743a458055d827
Mike <mike@mac-mini-01.local>
2014-06-17 11:57:18 -0400

    less javascript-y settings page

--- mht/js/settings.js
+++ mht/js/settings.js
@@ -14,57 +14,75 @@ var settings = function() {
 
 		$('.faq-setting').click(function(){
 
-			$('#art-loading').removeClass('o1').addClass('o0');
-			setTimeout(function() {
-				// Remove the Loading spinner icon...
-				$('#art-loading').remove();
-				$('#art-settings').html(Pages.faq).removeClass('o0').addClass('o1');
-				$('#art-faq').removeClass('o0').addClass('o1').show();
+			$('#art-settings').hide();
+			$('#art-faq').show();
+			// $('#page_quiz').show();
+			// $('#page_settings').hide();
+
+			// $('#art-loading').removeClass('o1').addClass('o0');
+			// setTimeout(function() {
+			// 	// Remove the Loading spinner icon...
+			// 	$('#art-loading').remove();
+			// 	$('#art-settings').html(Pages.faq).removeClass('o0').addClass('o1');
+				$('#art-faq').removeClass('o0').addClass('o1');
 
 				$('.cancel').click(function(){
 					
-					settings();
+					$('#art-faq').hide();
+					$('#art-settings').show();
+					
+
+					// settings();
 				});
 
-			}, 250);
+			// }, 250);
 		});
 
 
 		$('.about-setting').click(function(){
 
-			$('#art-loading').removeClass('o1').addClass('o0');
-			setTimeout(function() {
-				// Remove the Loading spinner icon...
-				$('#art-loading').remove();
-				$('#art-settings').html(Pages.about).removeClass('o0').addClass('o1');
-				$('#art-about').removeClass('o0').addClass('o1').show();
+				$('#art-settings').hide();
+					$('#art-about').show();
 
+			// $('#art-loading').removeClass('o1').addClass('o0');
+			// setTimeout(function() {
+				// Remove the Loading spinner icon...
+				// $('#art-loading').remove();
+				// $('#art-settings').html(Pages.about).removeClass('o0').addClass('o1');
+				// $('#art-about').removeClass('o0').addClass('o1').show();
+					$('#art-about').removeClass('o0').addClass('o1');
 				$('.cancel').click(function(){
-					alert("CLICKED");
-					settings();
+					// alert("CLICKED");
+					// settings();
+					$('#art-about').hide();
+					$('#art-settings').show();
+					
 				});
 
 				$('.about-contact').click(function(){
 						window.location="mailto:pathadmin@sunnybrook.ca";
 				});
 
-			}, 250);
+			// }, 250);
 		});
 
 		$('.change-setting').click(function(){
-
-			$('#art-loading').removeClass('o1').addClass('o0');
-			setTimeout(function() {
-				// Remove the Loading spinner icon...
-				$('#art-loading').remove();
-				$('#art-settings').html(Pages.changePassword).removeClass('o0').addClass('o1');
-				$('#art-about').removeClass('o0').addClass('o1').show();
+$('#art-settings').hide();
+				$('#art-change').show();
+					
+			// $('#art-loading').removeClass('o1').addClass('o0');
+			// setTimeout(function() {
+			// 	// Remove the Loading spinner icon...
+			// 	$('#art-loading').remove();
+				$('#art-change').removeClass('o0').addClass('o1');
+			// 	$('#art-about').removeClass('o0').addClass('o1').show();
 
 				$('.cancel').click(function(){
-					settings();
+					$('#art-change').hide();
+					$('#art-settings').show();
 				});
 
-			}, 250);
+			// }, 250);
 		});
 
 //	}, 250);
